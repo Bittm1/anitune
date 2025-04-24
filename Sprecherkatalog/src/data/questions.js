d5bfa81aaed56a435f5e3558e3e8cea6e887582a
Bittm <cryptomacki@gmail.com>
2025-04-24 12:16:59 +0200

    Fix question audio filenames and paths

--- Sprecherkatalog/src/data/questions.js
+++ Sprecherkatalog/src/data/questions.js
@@ -2,17 +2,17 @@ const questions = [
   {
     key: "q1",
     text: "Seit wann sprichst du?",
-    audio: "/audio/tobiasmueller/Seit wann sprichst du.mp3"
+    audio: "/audio/tobiasmueller/seit_wann_sprichst_du.mp3"
   },
   {
     key: "q2",
     text: "Wie bist du Synchronsprecher geworden?",
-    audio: "/audio/tobiasmueller/Wie bist du Synchronsprecher geworden.mp3"
+    audio: "/audio/tobiasmueller/wie_bist_du_synchronsprecher_geworden.mp3"
   },
   {
     key: "q3",
     text: "Was liebst du an deinem Beruf?",
-    audio: "/audio/tobiasmueller/Was liebst du an deinem Beruf.mp3"
+    audio: "/audio/tobiasmueller/was_liebst_du_an_deinem_beruf.mp3"
   }
 ];
 
